f677c04f6aa578896d23df0dd93abd4e6a8568e8
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-18 02:00:37 -0700

    Problem 15

---
+++ Problem15/Program.cs
@@ -0,0 +1,17 @@
+﻿void DayOfTheWeek(int dayNum)
+{
+    if (dayNum == 6 || dayNum == 7)
+    {
+        Console.WriteLine("It's a weekend bruh!");
+    }
+    else if (dayNum < 6 && dayNum != 0)
+    {
+        Console.WriteLine("Have to work, business day!");
+    }
+    else
+    {
+        Console.WriteLine("Not a weekday, don't mess with me!");
+    }
+}
+int dayNum = 5;
+DayOfTheWeek(dayNum);
