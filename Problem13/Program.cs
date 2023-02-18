dd4a849eabfd0236db3f55023ccc611edb4661bb
azamat Khadikov <khadikovazamat@gmail.com>
2023-02-18 01:15:48 -0700

    Problem13

---
+++ Problem13/Program.cs
@@ -0,0 +1,12 @@
+﻿int number = 645; //78, 32679
+string num = Convert.ToString(number);
+if (num.Length < 3)
+{
+    Console.WriteLine("No 3rd number");
+}
+else
+{
+    Console.WriteLine(num[2]);
+}
+
+
